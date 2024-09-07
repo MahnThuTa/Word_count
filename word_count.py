7be4a4b041b9192547352a926699ba35b75aaf38
Rivan <shinewathan23@gmail.com>
2024-09-07 14:57:46 +0630

    message

--- word_count.py
+++ word_count.py
@@ -1,2 +1,14 @@
+import re
+from collections import Counter
+
 def count_words(sentence):
-    pass
+    sentence = sentence.lower().replace("_", "-")
+    words = re.findall(r"\b\w+(?:'\w+)?\b", sentence)
+    
+    # Use Counter to count occurrences of each word
+    word_count = Counter(words)
+    
+    return word_count
+
+sentence = input("Enter a sentence: ")
+print(count_words(sentence))
