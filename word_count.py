66bc028b9e0a37c5265cef4c55fa0ed360a10907
Rivan <shinewathan23@gmail.com>
2024-09-07 15:05:25 +0630

    change names

--- word_count.py
+++ word_count.py
@@ -3,10 +3,10 @@ from collections import Counter
 
 def count_words(sentence):
     sentence = sentence.lower().replace("_", "-")
-    words = re.findall(r"\b\w+(?:'\w+)?\b", sentence)
+    w = re.findall(r"\b\w+(?:'\w+)?\b", sentence)
     
     # Use Counter to count occurrences of each word
-    word_count = Counter(words)
+    word_count = Counter(w)
     
     return word_count
 
