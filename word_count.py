77dd31a21c91a4cb18688c92759a386f8bf91a34
Kuhiro <119740529+MahnThuTa@users.noreply.github.com>
2024-09-07 17:35:14 +0630

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -17,18 +17,7 @@ print(count_words(sentence))
 
 
 # mahn_branch
-from collections import Counter
 
-def count_words(sentence: str):
-    sentence = sentence.lower().replace("_", "-")
-    
-    words = re.findall(r"\b\w+(?:'\w+)?\b", sentence)
-    
-    word_count = Counter(words)
-    
-    return dict(word_count)
-
-print(count_words(sentence=input("Enter sentence: ")))
 
 #--------------------------------------------------------
 
